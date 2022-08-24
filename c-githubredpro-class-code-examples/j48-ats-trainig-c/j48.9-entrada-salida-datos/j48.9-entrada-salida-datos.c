4bfecb6463ba428ed06b898f3d769696035d289a
dev-githubpro <github@githubred.com>
2022-08-24 06:17:32 +0200

    folder 8

--- c-githubredpro-class-code-examples/j48-ats-trainig-c/j48.9-entrada-salida-datos/j48.9-entrada-salida-datos.c
+++ c-githubredpro-class-code-examples/j48-ats-trainig-c/j48.9-entrada-salida-datos/j48.9-entrada-salida-datos.c
@@ -8,45 +8,46 @@ int main (){ // Funcion principal
 	// Operacion 1 salida de muchos datos
 	int 	a = 10;
 	float	b = 15.5; // (.2f) determina en nuemro de flotantes que apareceran despues del 15
-	char	c = 'e'; // Siempre comillas simples para las operaciones de tipo caractere, recuerda que la char solo devuelve el ultimo caractere de una cadena de string
+	char	f = 'e'; // Siempre comillas simples para las operaciones de tipo caractere, recuerda que la char solo devuelve el ultimo caractere de una cadena de string
 
 	// Pinta por pantalla los datos de operacion1
-	printf("El valoe de suma es %i \n el valor de float es %.2f \n el valor d e char es %c \n", a, b, c);
-	printf(" \n");
+	//	printf("El valoe de int es %i \n el valor de float es %.2f \n el valor d e char es %c \n", a, b, f);
+	
+		printf("El valor de int es %i \n", a);
+		printf("El valor de float es %.2f \n ", b);
+		printf("El valoe de char es %c \n", f);
+	printf(" \n");														// Salto de linea
+	/*-----------------------------------------------------------------------------------------------------------------------------------------------------*/
 		
-	//--------------------------------------		
 	// Operacion 2 entrada y salida de un numeor entero
 	int 	d; // espacio en memoria
-	float	e; // (.2f) determina en nuemro de flotantes que apareceran despues del 15
-	char	cadena; // Siempre comillas sinples para las operaciones de codigo
-	/*
- 	// Entrada de datos para un numero entrero int d
-	printf("Digite el valor de la variable a : ");
-	scanf("%i", &d); // lo que hacemos es pedir por pantalla el valor y el tipo (%i) de de la variable (a) y en que posicion de memoria lo vamos guardar (&d) 
-	printf("El valor de la variable (a) es un numero entero : %i", d);
-	printf(" \n");
-	printf(" \n");
+
+	printf("Digite el valor de la variable ( d ) : ");// Pregunta y pinta por pantalla
+	scanf("%i", &d); // lo que hacemos es pedir por pantalla el valor y el tipo (%i) de de la variable (d) y en que posicion de memoria lo vamos guardar (&d) 
+	printf("El valor de la variable ( d ) es un numero entero : %i", d);
+	printf(" \n");															// Salto de linea
+	printf(" \n");															// Salto de linea
 	
-	//--------------------------------------		
+	
 	// Operacion 3 entrada y salida de un numero flotante
-	// Entrada de datos para un numero flotante e
-	printf("Digite el valor de la variable e :");
-	scanf("%f", &e);
-	// printf("El valor de la variable (e) es un numero flotante %f", e);
-	printf("El valor de la variable (e) es un numero flotante %.2f", e);
-	printf(" \n");
-	printf(" \n");
-	*/
-	
-	// Entrada de dato par una cadena de caracteres
-	printf("Introduzca un charactere para la variable c : ");
-	scanf("%c", &cadena);
-	printf("el resultado para la variable %c es :", cadena); // Solo pinta un caractere
-
+	float	e;														
 		
+	printf("Digite el valor de la variable ( e ) :");						// Pregunta y pinta por pantalla
+	scanf("%f", &e);														// Entrada de datos para un numero flotante e
+	printf("El valor de la variable ( e ) es un numero flotante %.2f", e);	// printf("El valor de la variable (e) es un numero flotante %f", e);
+																			// (.2f) determina en nuemro de flotantes que apareceran despues del 15
+	printf(" \n");															// Salto de linea
+	printf(" \n");															// Salto de linea
 	
+	
+	// Operacion 4 entrada y salida de dato tipo char
+	char	c; // Siempre comillas sinples para las operaciones de codigo
+	
+	printf("Introduzca un charactere para la variable ( c ) : ");// Entrada de dato par una cadena de caracteres
+	scanf("%c", &c);
+	printf("El valor es  %c : ", c); // Solo pinta un caractere
 
-		return 0; // devuelve 0, es decir hemos terminado
+	return 0; // devuelve 0, es decir hemos terminado
 }
 
 
